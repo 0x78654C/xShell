e584a5a19f3c4fb4e1130e3ad11763a121147546
0x78654c <l2xguard@live.com>
2024-10-08 18:29:32 +0300

    fix new line on unknown command

--- src/main.c
+++ src/main.c
@@ -95,7 +95,7 @@ int main (int argc, char *argv[])
                  }
                  else 
                  {
-                    printf("Unknown command...");
+                    printf("Unknown command...\n");
                  }
             } 
         } 
